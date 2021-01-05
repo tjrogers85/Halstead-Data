5288edc424b7f709f89bb683c0428126e387b808
tjrogers85 <tjrogers85@gmail.com>
2021-01-04 19:27:40 -0500

    Initial Commit

---
+++ Halstead Data/Halstead_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Halstead_DataApp.swift
+//  Halstead Data
+//
+//  Created by TJ on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Halstead_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
